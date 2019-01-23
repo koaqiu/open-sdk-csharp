ccb51a80edc8da1b3d51b7e9eb9121912f8c69d3
koaqiu <koaqiu@gmail.com>
2019-01-23 14:37:43 +0800

    更新测试项目 appid等参数 从配置文件中读取

--- TestCore/Program.cs
+++ TestCore/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using Microsoft.Extensions.Configuration;
 using YZOpenSDK;
+using YZOpenSDK.Entrys;
+using YZOpenSDK.Util;
 
 namespace TestCore {
     class Program {
         static void Main(string[] args) {
-            
-            var auth = new Token("7940650b133c322f8e84919f3938e4b7");
+            var config = getConfig();
+            var auth = new Token(config, new FileCache());
+
             YZClient yzClient = new DefaultYZClient(auth);
             var dict = new Dictionary<string, object>
             {
@@ -21,6 +26,17 @@ namespace TestCore {
             //var result = yzClient.Invoke("youzan.item.create", "3.0.0", "post", dict, files);
             var result = yzClient.Invoke("youzan.items.onsale.get", "3.0.0", "get", dict, null);
             Console.WriteLine(result);
+
+        }
+
+        static YouzanConfig getConfig() {
+            var conf = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile("appsettings.Development.json", true, true)
+                .Build();
+            return conf.GetSection("youzan").Get<YouzanConfig>();
         }
     }
+
 }
