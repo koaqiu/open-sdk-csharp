8e2d09e30d632069cb2ed89823166d1b9f51a4f3
koaqiu <koaqiu@gmail.com>
2019-01-26 09:48:10 +0800

    callback 控制器

--- open_sdk_csharp/xBei/Youzan/Services/CallbackController.cs
+++ open_sdk_csharp/xBei/Youzan/Services/CallbackController.cs
@@ -1,30 +1,50 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using System;
 using System.Threading.Tasks;
 using YZOpenSDK.Entrys;
-using YZOpenSDK.xBei.Security;
 using YZOpenSDK.xBei.Youzan.Entrys;
 
 namespace YZOpenSDK.xBei.Youzan.Services {
-	[Route("api/v1/[controller]")]
-	[ApiController]
-	public class CallbackController : ControllerBase {
-		private readonly YouzanConfig _config;
-		public CallbackController(IOptions<YouzanConfig> config) {
-			_config = config.Value;
-		}
-		[HttpPost]
-		public ActionResult Post([FromBody] MsgPushEntity msg) {
-			handler(msg);
-			return new JsonResult(new { code = 0, msg = "success" });
-		}
-		private void handler(MsgPushEntity msg) {
-			Task.Run(() => handlerAsync(msg));
-		}
-		private void handlerAsync(MsgPushEntity msg) {
-			if (msg.Test || msg.Mode != 1) return;
-			if (!msg.CheckSign(_config)) return;
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CallbackController : ControllerBase {
+        private readonly YouzanConfig _config;
+        public CallbackController(IOptions<YouzanConfig> config) {
+            _config = config.Value;
+        }
+        /// <summary>
+        /// 认证回调
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("authorize")]
+        public async Task<ActionResult> Authorize() {
+            throw new NotImplementedException();
+        }
 
-		}
-	}
+        /// <summary>
+        /// 接收推送信息
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <returns></returns>
+        [HttpPost("msg")]
+        public async Task<ActionResult> PushMsg([FromBody] MsgPushEntity msg) {
+            await Task.Run(() => {
+                try {
+                    handlerAsync(msg);
+                } catch {
+                    // do nothing
+                }
+            });
+            return new JsonResult(new { code = 0, msg = "success" });
+        }
+        private void handler(MsgPushEntity msg) {
+            Task.Run(() => handlerAsync(msg));
+        }
+        private void handlerAsync(MsgPushEntity msg) {
+            if (msg.Test || msg.Mode != 1) return;
+            if (!msg.CheckSign(_config)) return;
+
+        }
+    }
 }
